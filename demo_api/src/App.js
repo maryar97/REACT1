1b7716548f5e554571dd22718282188cdf1c71ec
maryar97 <marydiva201170@gmail.fr>
2023-12-13 15:27:59 +0100

    notif

--- demo_api/src/App.js
+++ demo_api/src/App.js
@@ -1,8 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [Liste, setListe] = useState([]);
+
+  useEffect(()=>{
+    console.log("Chargement du composant...");
+
+    axios.get("http://127.0.0.1:8000/api/clients").then(
+      (data)=>{
+        console.log("les données sont servies...")
+        console.log(data)
+      }
+    )
+
+  },[])
+  
   return (
     <div>
-      
-    </div>
+      App test 
+      <hr /> 
+      {
+        Liste.map((client)=>(
+          <div> 
+            {client.nom}
+          </div>
+        ))
+      }
+    </div> 
   );
 }
 
