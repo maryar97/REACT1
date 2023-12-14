53322b8960de0e884195c0dfcd2c39be9467a3d0
maryar97 <marydiva201170@gmail.fr>
2023-12-14 15:30:15 +0100

    notif

--- demo_api/src/App.js
+++ demo_api/src/App.js
@@ -5,8 +5,26 @@ function App() {
 
   const [Liste, setListe] = useState([]);
 
+  const loadData = () => {
+    axios.get("https://127.0.0.1:8000/api/clients",
+    {
+      headers: {
+        Accept: "application/json"
+      }
+    }
+  ).then(
+    (reponse) => {
+      console.log("les données sont servies...")
+      console.log(reponse.data)
+      setListe(reponse.data)
+    }
+  )
+  }
+
   useEffect(()=>{
     console.log("Chargement du composant...");
+
+    loadData();
 
     // fetch("https://127.0.0.1:8000/api/clients")
     // .then((r)=>r.json())
@@ -16,25 +34,22 @@ function App() {
     //}
   //)
 
-    axios.get("https://127.0.0.1:8000/api/clients",
-      {
-        headers: {
-          Accept: "application/json"
-        }
-      }
-    ).then(
-      (reponse) => {
-        console.log("les données sont servies...")
-        console.log(reponse.data)
-        setListe(reponse.data)
-      }
-    )
+
 
   },[])
+
+  const handleClick = () =>{
+    axios.post("https://127.0.0.1:8000/api/clients", {nom: "111", prenom: "222"}) .then((data)=>{
+      console.log(data);
+
+        loadData();
+    });
+  }
   
   return (
     <div>
       App test
+      <button onClick={handleClick}>Ajouter</button>
       <hr /> 
       {
         Liste.map((client, index)=>(
